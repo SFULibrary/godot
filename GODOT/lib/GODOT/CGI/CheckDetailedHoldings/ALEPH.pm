6c43c9a77796f57485b66c463ddbbb42fa046808
Kristina Long <kristinahelenlong@gmail.com>
2006-09-13 19:00:36 +0000

    ALEPH module added for Brandon.
    git-svn-id: file:///home/zlocal/godot/trunk@56 f3e99952-5313-0410-b89c-a2f6a25015b6

---
+++ GODOT/lib/GODOT/CGI/CheckDetailedHoldings/ALEPH.pm
@@ -0,0 +1,28 @@
+package GODOT::CGI::CheckDetailedHoldings::ALEPH;
+##
+## Copyright (c) 2005, Kristina Long, Simon Fraser University
+##
+use Exporter;
+use GODOT::Debug;
+use GODOT::String;
+use GODOT::CGI::CheckDetailedHoldings;
+
+@ISA = qw(Exporter GODOT::CGI::CheckDetailedHoldings);
+
+use strict;
+
+sub button {
+    my($self) = @_;
+
+    use GODOT::PageElem::Button;
+    my $button = new GODOT::PageElem::Button;    
+
+    $button->label($self->_button_text);
+    $button->action('catalogue_interface_action');
+    $button->param("=" . $self->{'site'});
+
+    return $button;
+}
+
+1;
+
