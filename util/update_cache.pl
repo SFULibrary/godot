9df4f10ee62e3da5a2f153e02e6809fc60475316
Kristina Long <kristinahelenlong@gmail.com>
2006-10-04 01:23:15 +0000

    Changed 'update_cache.pl' to a script that will be run directly by the user as part of the install process instead of a script that is called by 'install.pl'.
    This was done for two reasons:
    
        1) it makes it easier to ensure that the correct user is running the script;
        2) makes the user doing the install aware of the profile cache set up.
    
    
    
    git-svn-id: file:///home/zlocal/godot/trunk@70 f3e99952-5313-0410-b89c-a2f6a25015b6

--- util/update_cache.pl
+++ util/update_cache.pl
@@ -1,4 +1,6 @@
 #!/usr/local/bin/perl
+##
+## GODOTConfig profile cache update script.
 
 use FindBin qw($Bin);
 
@@ -8,12 +10,31 @@ use strict;
 
 use Data::Dumper;
 use Time::HiRes qw(gettimeofday);
+use Term::ReadLine;
+
+use Install::GODOT;
+use GODOT::String;
 use GODOT::Debug;
-use GODOTConfig::Configuration;
 use GODOTConfig::Cache;
 use GODOTConfig::Sites;
 
+my $TRUE  = 1;
+my $FALSE = 0;
+
+my $term = new Term::ReadLine 'GODOT Profile Cache Update';
+
+&show_introduction;
+
+printw "Do you want to continue?";
+my $input = $term->readline('[Y/n]: ');
+if ($input =~ /^\s*n/i) {
+    exit;
+}
+
+printw "\nUpdating site profile cache ...\n";
+
 my @sites_from_database = GODOTConfig::Sites->site_keys;
+my @cache_files;
 
 foreach my $site (sort @sites_from_database) {
 
@@ -22,6 +43,8 @@ foreach my $site (sort @sites_from_database) {
     unless ($cache->write_to_cache_store($site)) {
         die "failed to write ", $site, " to cache"; 
     }
+
+    push @cache_files, $cache->filename;
 }
 
 ##
@@ -38,5 +61,46 @@ foreach my $site_in_store (@sites_in_cache_store) {
 
       GODOTConfig::Cache->delete_from_cache_store($site_in_store); 
   } 
-
 }
+
+
+##
+## Does the user want to change the ownership or permissions of the cache files.
+##
+
+printw "\nChanging profile cache ownership and permissions ...\n";
+
+&set_permissions([@cache_files], $term, $FALSE);
+
+
+
+
+
+sub show_introduction {
+    print "Updating the GODOT Profile File Cache\n";
+    print "==================================\n";
+    printw "The public interface to GODOT uses profile cache files instead of making direct calls to the ", 
+           "GODOT configuration database.\n\n",
+           "This script should be used to update the profile cache ", 
+           "after installing the demo profiles.\n\n"; 
+    printw "It can also be used to update the cache anytime modifications ", 
+           "are made to the profile database outside of the GODOT configuration tool.\n\n";
+}
+
+
+
+1;
+
+__END__
+
+
+
+
+
+
+
+
+
+
+
+
