3f9ac0798b1fe764ecc611f95113b520cd9326a5
Kristina Long <kristinahelenlong@gmail.com>
2011-01-08 03:47:08 +0000

    Added comments re encoding and debug statements using Data::Dump. CGI::script_name now returns query string as well so switch to using $ENV{'SCRIPT_NAME'}.
    git-svn-id: file:///home/zlocal/godot/trunk@316 f3e99952-5313-0410-b89c-a2f6a25015b6

--- GODOT/lib/GODOT/RefWorks/CGI.pm
+++ GODOT/lib/GODOT/RefWorks/CGI.pm
@@ -2,9 +2,15 @@ package GODOT::RefWorks::CGI;
 ## 
 ## Copyright (c) 2009, Kristina Long, Simon Fraser University
 ##
-## 
+## (30-oct-2010 kl) 
+## Incoming citation data is assumed to be encoded as latin1.  
+## Output of citation data to file (for input into Refworks) is also latin1. 
+## For now, no decoding or encoding is done by this package or by GODOT::RefWorks::RIS as input and output encoding is the same. 
+##
 use CGI qw(:standard :escape);
 use FileHandle;
+
+use Data::Dump qw(dump);
 
 use GODOT::Object;
 use GODOT::Debug;
@@ -51,7 +57,7 @@ sub run {
     my($self, $state) = @_;
 
     debug "-- run --";
-    debug $self->dump;
+    debug Data::Dump::dump($self);
 
     if (grep {$state eq $_} qw(import export)) {
         no strict;
@@ -76,32 +82,35 @@ sub import_citation {
     my($self) = @_;
     
     #### foreach my $param (param()) {  debug "<incoming>  $param = ", param($param); }
-    debug "-- import 1 --";
 
     debug "-- import 1 --";
-    debug $self->dump;
+    debug Data::Dump::dump($self);
 
     my $database = new GODOT::Database;
     $database->dbase(param('DBASE'));
     my $citation = new GODOT::Citation $database;
    
+    ##
+    ## (30-oct-2010 kl) 
+    ## Subroutine init_from_parsed_params does no decoding, so strictly speaking we should be decoding incoming parameters here.  However
+    ## manipulation of $citation object is minimal before we are again doing output which we want in latin1 anyways.... so leave for now.   
+    ##
     $citation->init_from_parsed_params();    
 
-    #### use Data::Dumper;
-    #### debug Dumper "\n\n", $citation, "\n";
+    #### debug "\n\n", Data::Dump::dump($citation), "\n";
 
     my $filter = new GODOT::RefWorks::RIS; 
 
     my $filtered = $filter->export_citation($citation);
 
     #### debug "\n---------- ris filtered ------------";
-    #### debug $filtered;
+    #### debug Data::Dump::dump($filtered);
     #### debug "\n------------------------------------\n";
 
     my $db = $database->dbase;
 
     debug "-- import 2 --";
-    debug $self->dump;
+    debug Data::Dump::dump($self);
 
     $self->generate_file_name;
 
@@ -109,18 +118,31 @@ sub import_citation {
         return header . start_html . "Unable to generate name for temporary file." . end_html;        
     }
   
-    debug "file:  ", $self->file;
-    debug "file_id:  ",  $self->file_id;
+    debug location_plus, "file:  ", $self->file;
 
     my $fh = new FileHandle;
     unless (open $fh, ('> ' . $self->file)) {
         return header . start_html . "Unable to open temporary file (" . $self->file . ") for writing." . end_html;        
     } 
 
-
     print $fh $filtered;
 
-    my $export_url = join('', 'http://', server_name, ':', server_port, script_name, '?state=export&id=', escape($self->file_id));
+    debug location_plus, "file_id:  ",  $self->file_id;
+
+    #### foreach my $tmp (keys %ENV) {
+    ####    debug location_plus, "ENV -- $tmp -- $ENV{$tmp}"; 
+    #### }
+    
+    ##
+    ## (09-dec-2010 kl) -- CGI::script_name now returns query string as well so switch to using $ENV{'SCRIPT_NAME'}  
+    ##
+    #### my $export_url = join('', 'http://', server_name, ':', server_port, script_name, '?state=export&id=', escape($self->file_id));
+   
+    my $export_url = join('', 'http://', server_name, ':', server_port, $ENV{'SCRIPT_NAME'}, '?state=export&id=', escape($self->file_id));
+
+
+    debug location_plus, "export_url:  ",  $export_url;
+
 
     ##
     ## (21-apr-2005 kl) - the vendor and database do not show up in refworks.  This is a problem for other 
@@ -137,7 +159,7 @@ sub import_citation {
 
     $rw_url = param('proxy') . $rw_url;
 
-    debug "rw_url:  $rw_url\n";
+    debug location_plus, "rw_url:  $rw_url\n";
 
     return redirect($rw_url);
 }
@@ -163,8 +185,13 @@ sub export_citation {
     } 
     
     my @filtered = <$fh>;    
-
-    return header('text/plain') . join('', @filtered);
+     
+    ##
+    ## (30-oct-2010 kl) -- header default is latin1 but add explicitly for clarity
+    ##
+    #### return header('text/plain') . join('', @filtered);
+    ####
+    return header(-type => 'text/plain', -charset => 'ISO-8859-1') . join('', @filtered);
 }
 
 ##
@@ -174,7 +201,7 @@ sub generate_file_name {
     my($self) = @_;
 
     debug "-- generate_file_name --";
-    debug $self->dump;
+    debug Data::Dump::dump($self);
 
     my($file, $file_id) = (aws($self->file_id)) ? get_tmp_file_new_id($DIR, $FILE_PREFIX, $FILE_SUFFIX, $FILE_RETRY, $FILE_ID_RAND_MAX)
                                                 : get_tmp_file_for_id($DIR, $FILE_PREFIX, $FILE_SUFFIX, $self->file_id);
