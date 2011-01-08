c1650cc12e953aad7c4faeae3158002caf04b3ef
Kristina Long <kristinahelenlong@gmail.com>
2011-01-08 21:06:44 +0000

    Added bib_record_read_correct_decode method which calls GODOT::Encode::bib_record_detect_encoding. If there is a problem with encoding record will be tossed out. Added logic to copy all fields to $bib_record_object_decoded using a decoded version of all non-control fields. This will cause problems if any MARC::Field or MARC::Record logic is used that assumes data is still encoded. Added GODOT::Normalize::normalize_for_catalogue_match to good_match and _clean_varying_form_title methods.
    git-svn-id: file:///home/zlocal/godot/trunk@332 f3e99952-5313-0410-b89c-a2f6a25015b6

--- GODOT/lib/GODOT/CatalogueHoldings/Record/Z3950.pm
+++ GODOT/lib/GODOT/CatalogueHoldings/Record/Z3950.pm
@@ -1,14 +1,16 @@
 package GODOT::CatalogueHoldings::Record::Z3950;
-
 ##
 ## Copyright (c) 2005, Kristina Long, Simon Fraser University
 ##
-
 use GODOT::CatalogueHoldings::Record;
 push @ISA, qw(GODOT::CatalogueHoldings::Record);
 
+use Data::Dump qw(dump);
+
 use GODOT::Debug;
 use GODOT::String;
+use GODOT::Normalize;
+use GODOT::Encode;
 
 use strict;
 
@@ -19,9 +21,9 @@ my $MARC_OTHER_STANDARD_ID_FIELD  = '024';
 my $MARC_TITLE_FIELD              = '245';
 my $MARC_VARYING_FORM_TITLE_FIELD = '246';
 
-
 my $FALSE = 0;
 my $TRUE  = 1;
+my $BLANK = ' ';
 
 sub bibliographic {
     my($self) = @_;
@@ -29,32 +31,103 @@ sub bibliographic {
     return $self->record;
 }
 
+##
+## (08-oct-2010 kl) 
+## - moved older version of logic from good_match
+##
+sub bib_record_read_correct_decode {
+    my($self) = @_;
+
+    use MARC::Record;
+    my $bib = $self->bibliographic;
+    my $rawdata = $bib->rawdata();     
+
+    my ($encoding, $try_to_read) = bib_record_detect_encoding($rawdata);
+
+    unless ($try_to_read) {
+        debug location_plus, "encoding is $encoding -- record is being tossed out";
+        return $FALSE;
+    }
+
+    my $leader_09 = substr($rawdata, 9, 1);
+    my $new_leader_09 = leader_09_for_encoding($encoding);
+ 
+    if ($leader_09 ne $new_leader_09) {
+        substr($rawdata, 9, 1) = $new_leader_09;
+        debug location_plus, "leader changed from '$leader_09' to '$new_leader_09'";
+    }
+
+    my $bib_record_object;
+    eval {  $bib_record_object = MARC::Record->new_from_usmarc($rawdata); };
+    my $eval_res = $@;
+    debug location_plus, "eval error -- $eval_res" if $eval_res;
+
+    unless (defined $bib_record_object) {
+        debug location_plus, "bibliographic record object could not be created";
+        return $FALSE;
+    }
+
+    ## 
+    ## (23-oct-2010)
+    ## If marc record was encoded in utf8 the $bib_record_object data will be in perl internal format.
+    ## If marc record was encoded in marc8 the $bib_record_oject data will *still* be encoded in marc8.
+    ##
+    #### debug '-----------------before------------------------------------';
+    #### debug location;
+    #### debug Data::Dump::dump($bib_record_object);
+    #### debug '-----------------------------------------------------';
+
+    my $bib_record_object_decoded = MARC::Record->new();
+
+    ##
+    ## Copy all fields to $bib_record_object_decoded using a decoded version of all non-control fields.
+    ## !!!! Below will cause problems if any MARC::Field or MARC::Record logic is used that assumes data is still encoded.  However this should be ok for now for our purposes. !!!
+    ##    
+    my @cloned_fields;
+    foreach my $field ($bib_record_object->fields()) {
+        my $cloned_field;
+        ##
+        ## -are we likely to have other than ascii data in control fields ??
+        ##
+        if ($field->is_control_field) {                      
+            $cloned_field = $field->clone;
+        }
+        else {
+            $cloned_field = decode_marc_record_field_clone($bib_record_object, $field, $encoding);        
+        }
+        push @cloned_fields, $cloned_field;
+    }
+
+    $bib_record_object_decoded->append_fields(@cloned_fields);
+
+    ##
+    ## Data in all non-control fields should now be in perl internal format.
+    ##
+    #### debug '-----------------after------------------------------------';
+    #### debug location;
+    #### debug Data::Dump::dump($bib_record_object_decoded);
+    #### debug '-----------------------------------------------------';
+
+    $self->bib_record_object($bib_record_object_decoded);
+    return $TRUE;
+}
+
 sub good_match {
     my($self, $db_config, $citation) = @_;
 
-    my $bib = $self->bibliographic;
-    use MARC::Record;
-    my $rec = MARC::Record->new_from_usmarc($bib->rawdata());
-    unless (defined $rec) {
-        debug location, ":  bibliographic record not defined";
-        return ($FALSE, '');
-    }
+    #### report_location;
 
-    ##
-    ## -(12-mar-2005 kl) - MARC21 records can either use UTF-8 or MARC-8 encoding.  Assume for now
-    ##                     that all are MARC-8.  Later look at leader-09 to check (will contain blank for
-    ##                     MARC-8 and 'a' for UTF-8).  Will also need to write a GODOT::String::normalize_utf8 
-    ##                     function.      
+    my $bib_record_object = $self->bib_record_object;
+
+    #### debug '-----------------------------------------------------';
+    #### debug location;
+    #### debug Data::Dump::dump($bib_record_object);
+    #### debug '-----------------------------------------------------';
+
     ##
     ## -remove all leading and trailing white space so can later test for null string instead of all-white-space
     ##
-    
-    ##
-    ## (01-jul-2020 kl) -- GODOT::String in repository has new 'normalize' name instead of old 'normalize_latin1';
-    ##
-    #### my $citation_title =  trim_beg_end(&GODOT::String::normalize_latin1($citation->parsed('TITLE')));
-    my $citation_title =  trim_beg_end(&GODOT::String::normalize($citation->parsed('TITLE')));
-
+    my $citation_title =  normalize_for_catalogue_match($citation->parsed('TITLE'));
 
     ##
     ## (14-feb-2003 kl) - added logic to make ISSN and ISBN uppercase
@@ -77,29 +150,22 @@ sub good_match {
     if (scalar(@citation_isbns) == 2) {
         $citation_isbn_converted = $citation_isbns[1];
         $citation_isbn_converted =~ s#[\055\s]##g;                          ## remove hyphen and white space
-	$citation_isbn_converted = uc($citation_isbn_converted);
+	    $citation_isbn_converted = uc($citation_isbn_converted);
     }
 
     ##
-    ## (03-jun-1999 kl) - need to add logic for some more marc title fields  -- to test see
-    ##                    Cindy's 28-may-1999 note
-    ##                    (210-a, 212-a, 214-a, 222-a, 240-a, 242-ab)                     
+    ## (03-jun-1999 kl) - need to add logic for some more marc title fields  (210-a, 212-a, 214-a, 222-a, 240-a, 242-ab)                     
     ##
-    ##
-
     my %subroutines = ($MARC_ISBN_FIELD               => '_clean_isbn', 
                        $MARC_OTHER_STANDARD_ID_FIELD  => '_clean_isbn',
                        $MARC_ISSN_FIELD               => '_clean_issn', 
                        $MARC_TITLE_FIELD              => '_clean_title', 
                        $MARC_VARYING_FORM_TITLE_FIELD => '_clean_varying_form_title');
-
     my %clean;
 
-    my $leader = $rec->leader;
+    my $leader = $bib_record_object->leader;
 
-    foreach my $marc_field ($MARC_ISBN_FIELD, $MARC_OTHER_STANDARD_ID_FIELD, 
-                            $MARC_ISSN_FIELD, 
-                            $MARC_TITLE_FIELD, $MARC_VARYING_FORM_TITLE_FIELD) {
+    foreach my $marc_field ($MARC_ISBN_FIELD, $MARC_OTHER_STANDARD_ID_FIELD, $MARC_ISSN_FIELD, $MARC_TITLE_FIELD, $MARC_VARYING_FORM_TITLE_FIELD) {
 
         $clean{$marc_field} = [];
 
@@ -108,25 +174,24 @@ sub good_match {
             return ($FALSE, '');
         }
 
-        foreach my $field ($rec->field($marc_field)) {
+        foreach my $field ($bib_record_object->field($marc_field)) {
             no strict 'refs';
-            my @cleaned_up = &{$subroutines{$marc_field}}($field);
+            my @cleaned_up = &{$subroutines{$marc_field}}($field, $leader);
             use strict;
             push @{$clean{$marc_field}}, @cleaned_up;
-	}
+	     }
     }
 
-    if (0) {                         
-        debug "-----------------------------------";
-        debug "from citation ... ISBN:  $citation_isbn, ISBN CONVERTED:  $citation_isbn_converted, ISSN:  $citation_issn, TITLE:  $citation_title";
-        debug "from catalogue ..."; 
-        debug "leader:  ", $leader; 
-        foreach (@{$clean{$MARC_ISBN_FIELD}}, @{$clean{$MARC_OTHER_STANDARD_ID_FIELD}})   { debug "isbn:  $_"; }
-        foreach (@{$clean{$MARC_ISSN_FIELD}})   { debug "issn:  $_"; }
-        foreach (@{$clean{$MARC_TITLE_FIELD}})  { debug "title 245:  $_"; }
-        foreach (@{$clean{$MARC_VARYING_FORM_TITLE_FIELD}}) { debug "title 246:  $_"; }
-        debug  "------------------------------------";
-    }
+                             
+    #### debug "-----------------------------------";
+    #### debug "from citation ... ISBN:  $citation_isbn, ISBN CONVERTED:  $citation_isbn_converted, ISSN:  $citation_issn, TITLE:  $citation_title";
+    #### debug "from catalogue ..."; 
+    #### debug "leader:  ", $leader; 
+    #### foreach (@{$clean{$MARC_ISBN_FIELD}}, @{$clean{$MARC_OTHER_STANDARD_ID_FIELD}})   { debug "isbn:  $_"; }
+    #### foreach (@{$clean{$MARC_ISSN_FIELD}})   { debug "issn:  $_"; }
+    #### foreach (@{$clean{$MARC_TITLE_FIELD}})  { debug "title 245:  $_"; }
+    #### foreach (@{$clean{$MARC_VARYING_FORM_TITLE_FIELD}}) { debug "title 246:  $_"; }
+    #### debug  "------------------------------------";
 
     return ($FALSE, '') unless $self->leader_match($leader, $citation);
 
@@ -139,7 +204,6 @@ sub good_match {
     my @isbns = (@{$clean{$MARC_ISBN_FIELD}}, @{$clean{$MARC_OTHER_STANDARD_ID_FIELD}});
 
     my $num_record_isbn = scalar @isbns;
-
     my $num_record_issn = scalar @{$clean{$MARC_ISSN_FIELD}};
 
     ##
@@ -161,10 +225,6 @@ sub good_match {
         }
     }
 
-    #### debug "citation_issn:  $citation_issn";
-    #### debug "num_record_issn:  $num_record_issn";
-
-
     if ($citation_issn && $num_record_issn)   {
 
         if (grep {$citation_issn  eq $_} @{$clean{$MARC_ISSN_FIELD}}) {
@@ -174,7 +234,6 @@ sub good_match {
         else {
             return ($FALSE, '');
         }
-
     }
 
     if (($citation_title) && (grep {$citation_title eq $_} @{$clean{$MARC_TITLE_FIELD}})) { 
@@ -192,7 +251,7 @@ sub good_match {
 }
 
 sub _clean_isbn {
-    my($field) = @_;
+    my($field, $leader) = @_;
 
     my @cleaned_up;
  
@@ -213,7 +272,7 @@ sub _clean_isbn {
 
 
 sub _clean_issn {
-    my($field) = @_;
+    my($field, $leader) = @_;
 
     my @cleaned_up;
 
@@ -239,43 +298,42 @@ sub _clean_issn {
 
 
 sub _clean_title {
-    my($field) = @_;
+    my($field, $leader) = @_;
 
     my $title;
     my @cleaned_up;
 
     foreach my $subfield ($field->subfields) {
-	next unless defined $subfield;
+	    next unless defined $subfield;
                      
-	my($code, $data) = @{$subfield};      
+	    my($code, $data) = @{$subfield};      
 
         if ($code eq 'a') {
-            $title =  trim_beg_end(normalize_marc8($data));
+            $title =  normalize_for_catalogue_match($data, $leader);
             push(@cleaned_up, $title) unless aws($title);
-         }
-         elsif ($code eq 'b') {
+        }
+        elsif ($code eq 'b') {
+            my $remainder = normalize_for_catalogue_match($data, $leader);
 
-             my $remainder = trim_beg_end(normalize_marc8($data));
-
-             ##
-             ## -assume that 245-a, if it exists, will always come before 245-b
-             ##
-             if ($remainder) {
-                 if ($title) { push @cleaned_up, "$title $remainder"; }
-                 else        { push @cleaned_up, $remainder;          }
-             }
+            ##
+            ## -assume that 245-a, if it exists, will always come before 245-b
+            ##
+            if ($remainder) {
+                if ($title) { push @cleaned_up, "$title $remainder"; }
+                else        { push @cleaned_up, $remainder;          }
+            }
         }
     }
+
     return @cleaned_up;
 }
-
 
 ##
 ## ## variant forms of title
 ##
 
 sub _clean_varying_form_title {
-    my($field) = @_;
+    my($field, $leader) = @_;
 
     my @cleaned_up;
 
@@ -285,7 +343,7 @@ sub _clean_varying_form_title {
        my($code, $data) = @{$subfield};
 
        if ($code eq 'a') {
-           my $title = trim_beg_end(normalize_marc8($data));
+           my $title = normalize_for_catalogue_match($data, $leader);
            push(@cleaned_up, $title) unless aws($title);
        }
    }    
@@ -317,9 +375,9 @@ sub leader_match {
 	if ($citation->is_journal) {
             return $FALSE unless (grep {$leader_07 eq $_} @leader_07_for_journal);
         }
-	else {
+	    else {
             return $FALSE if (grep {$leader_07 eq $_} @leader_07_for_journal);
-	}		
+	    }		
     } 
 
     return $TRUE;   
