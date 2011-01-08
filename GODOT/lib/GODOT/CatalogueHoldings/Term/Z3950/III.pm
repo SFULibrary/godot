830ccb95c0292cf1115a1cf2d82a26356a95bb2f
Kristina Long <kristinahelenlong@gmail.com>
2011-01-08 22:16:01 +0000

    Added comment.
    git-svn-id: file:///home/zlocal/godot/trunk@340 f3e99952-5313-0410-b89c-a2f6a25015b6

--- GODOT/lib/GODOT/CatalogueHoldings/Term/Z3950/III.pm
+++ GODOT/lib/GODOT/CatalogueHoldings/Term/Z3950/III.pm
@@ -9,6 +9,7 @@ use GODOT::CatalogueHoldings::Term::Z3950;
 @ISA = qw(GODOT::CatalogueHoldings::Term::Z3950);
 
 use GODOT::String;
+use GODOT::Debug;
 
 use strict;
 
@@ -22,16 +23,22 @@ sub title {
     $self->Term($title);
 }
 
-sub strip_trailing_year_title {
-    my($self, $title, $strip_apostrophe_s, $is_journal) = @_;
-
-    $self->SUPER::strip_trailing_year_title($title, $strip_apostrophe_s, $is_journal);
-
-    $title = $self->Term;
-    $title = "\"$title\"";                                   ## -add quotes
-    return $self->Term($title);
-}
-
-
+##
+## (11-oct-2010 kl) vendor specific title method now called in GODOT::CatalogueHolding::Term::strip_trailing_year_title 
+##                  so no need to have vendor specific strip_trailing_year_method below
+##
+#### sub strip_trailing_year_title {
+####    my($self, $title, $strip_apostrophe_s, $is_journal) = @_;
+####
+####    $self->SUPER::strip_trailing_year_title($title, $strip_apostrophe_s, $is_journal);
+####
+####    $title = $self->Term;
+####    $title = "\"$title\"";                                   ## -add quotes
+####
+####    report_location;
+####    debug '>>>> Term:  ', $title;
+####    return $self->Term($title);
+#### }
+####
 
 1;
