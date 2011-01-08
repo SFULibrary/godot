26d6610f7f7ec04e6b2db5878d5e7a1fea7df6ed
Kristina Long <kristinahelenlong@gmail.com>
2011-01-08 22:14:21 +0000

    Remove brackets before calling title method.
    git-svn-id: file:///home/zlocal/godot/trunk@339 f3e99952-5313-0410-b89c-a2f6a25015b6

--- GODOT/lib/GODOT/CatalogueHoldings/Term/Z3950/SIRSI.pm
+++ GODOT/lib/GODOT/CatalogueHoldings/Term/Z3950/SIRSI.pm
@@ -7,22 +7,22 @@ use GODOT::CatalogueHoldings::Term::Z3950;
 @ISA = qw(GODOT::CatalogueHoldings::Term::Z3950);
 
 use GODOT::String;
+use GODOT::Debug;
 
 use strict;
 
 sub title {
     my($self, $title, $strip_apostrophe_s, $is_journal) = @_;
 
-    $self->SUPER::title($title, $strip_apostrophe_s, $is_journal);
-
     ## 
     ## (28-nov-2003 kl) - brackets in a search term result in errors for Net::Z3950 searching a Sirsi catalogue
     ##
-
-    $title =~ s#\(# #g;                                       ## -get rid of brackets
+    $title =~ s#\(# #g;                                     
     $title =~ s#\)# #g;
 
-    $self->Term($title);
+    $title = $self->SUPER::title($title, $strip_apostrophe_s, $is_journal);
+
+    return $self->Term($title);
 }
 
 
